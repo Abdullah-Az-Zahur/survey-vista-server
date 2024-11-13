0cc495bad3ed185d456b29212f13e5a6fc4b9063
Abdullah-Az-Zahur <abdullah.az.zahur@gmail.com>
2024-11-14 00:06:03 +0600

    Add : surveyor  middleware

--- middleware/verifyRole.js
+++ middleware/verifyRole.js
@@ -25,3 +25,29 @@ export const verifyAdmin = async (req, res, next) => {
     res.status(500).send({ message: "Internal server error" });
   }
 };
+
+// Verify Surveyor Middleware
+export const verifySurveyor = async (req, res, next) => {
+  try {
+    console.log("Verifying Surveyor...");
+    const user = req.user;
+
+    if (!user || !user.email) {
+      return res
+        .status(400)
+        .send({ message: "Invalid request: missing user email" });
+    }
+
+    const result = await getUserByEmail(user.email);
+    console.log(`Role: ${result?.role}`);
+
+    if (!result || result.role !== "surveyor") {
+      return res.status(401).send({ message: "Unauthorized access!" });
+    }
+
+    next();
+  } catch (error) {
+    console.error("Error in verifySurveyor:", error);
+    res.status(500).send({ message: "Internal server error" });
+  }
+};
